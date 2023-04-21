253c8b6e5dc8aa6e381478c3811ba274ad299d18
bksat90 <bksat90@gmail.com>
2023-04-21 19:59:55 +0100

    Hotellings T2

--- assignment.R
+++ assignment.R
@@ -1,9 +1,11 @@
 # install excel, dplyr packages
 install.packages("readxl")
 install.packages("dplyr")
+install.packages("ICSNP")
 # loading the libraries
 library(readxl)
 library(dplyr)
+library(ICSNP)
 
 # reading the file from the excel; columns only A:G have data
 myFile <- "C:/Users/bksat/Documents/mvs_assignment/Customers.xlsx"
@@ -33,15 +35,29 @@ sampleMeanVector
 
 
 # Greatest level of variation
-
+S <- var(quantVar)
+S
+# Answer s2 for Income and Score (2536.113758 and 808.227563 respectively)
+# are high. ie. Variance of these factors are high. So they have highest 
+# deviation from the mean
 
 ####### highest correlation
-# covariance matrix
-S <- var(quantVar)
 # correlation matrix
 R <- cor(S)
 R
 # Answer income and size have higher positive correlation 0.9506432
 
 
-####### 
+####### population mean vector is equal to given vector
+givenMean <- c(88.0, 50.0, 4.5, 3.2)
+
+HotellingsT2(quantVar, mu = givenMean)
+
+#data:  quantVar
+#T.2 = 0.060313, df1 = 4, df2 = 396, p-value = 0.9933
+#alternative hypothesis: true location is not equal to c(88,50,4.5,3.2)
+
+######### two-sample profile analysis plot
+# male group
+
+# female group
